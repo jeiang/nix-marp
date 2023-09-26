5063088b9c15348035f98498a96b79624f400eee
Aidan Pinard <aidan@aidanpinard.co>
2023-09-25 22:58:27 -0400

    Upgrade marp-cli to 3.3.0

--- marp-cli.nix
+++ marp-cli.nix
@@ -1,13 +1,12 @@
 { mkYarnPackage, fetchFromGitHub }:
-
 mkYarnPackage {
   name = "marp-cli";
-  version = "2.3.0";
+  version = "3.3.0";
   src = fetchFromGitHub {
     owner = "marp-team";
     repo = "marp-cli";
-    rev = "v2.3.0";
-    sha256 = "zk52vYWdPU+U7GIXjoHaVP7ZRXWmVyL/DcOaR2dmfps=";
+    rev = "v3.3.0";
+    sha256 = "sha256-/BJqNXIXeqHr6QsK0jO2GQEnLxn0/N5HFjad4Ox4LXk=";
   };
   buildPhase = ''
     export HOME=$(mktemp -d)
